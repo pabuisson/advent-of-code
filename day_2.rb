fe008228007378b6d46cb011be1cce29a2763a17
pabuisson <pierre-adrien.buisson@doctolib.com>
2021-12-03 21:14:58 +0100

    Day 2: part 2

--- day_2.rb
+++ day_2.rb
@@ -7,18 +7,24 @@ class Day2
     @data = format_data(data)
   end
 
-  def compute_part_1!
+  def part_1_position
     position = Position.new
-    data.each do |instruction|
-      move = Move.new(instruction)
-      position.move!(move)
-    end
+    data.each { |instruction| position.move!(Part1::Move.new(instruction)) }
+    position.to_h
+  end
 
-    puts "Final position: #{position.to_h}"
-    position.x * position.depth
+  def compute_part_1!
+    part_1_position[:x] * part_1_position[:depth]
+  end
+
+  def part_2_position
+    sub = Part2::Submarine.new
+    data.each { |instruction| sub.move!(instruction) }
+    sub.position.to_h
   end
 
   def compute_part_2!
+    part_2_position[:x] * part_2_position[:depth]
   end
 
   private
@@ -53,28 +59,78 @@ class Day2
     attr_writer :x, :depth
   end
 
-  class Move
-    attr_reader :x, :depth
+  module Part1
+    class Move
+      attr_reader :x, :depth
 
-    def initialize(instruction)
-      @x = 0
-      @depth = 0
-      parse(instruction)
+      def initialize(instruction)
+        @x = 0
+        @depth = 0
+        parse(instruction)
+      end
+
+      private
+
+      attr_writer :x, :depth
+
+      def parse(instructions)
+        direction, units = instructions.split(' ')
+        case direction
+        when 'forward'
+          self.x = units.to_i
+        when 'down'
+          self.depth = units.to_i
+        when 'up'
+          self.depth = -units.to_i
+        end
+      end
+    end
+  end
+
+  module Part2
+    # Submarine objects holds its position, but also the aim value
+    # Aim value is then passed as a param when we move forward to
+    # compute how much the move forward will affect the depth, depending on the aim
+    class Submarine
+      attr_reader :aim, :position
+
+      def initialize
+        @position = Position.new(x: 0, depth: 0)
+        @aim = 0
+      end
+
+      def move!(instruction)
+        direction, units = instruction.split(' ')
+        case direction
+        when 'up'
+          self.aim -= units.to_i
+        when 'down'
+          self.aim += units.to_i
+        when 'forward'
+          move = Part2::MoveForward.new(units: units, aim: aim)
+          position.move!(move)
+        end
+      end
+
+      private
+
+      attr_writer :aim
     end
 
-    private
+    # Moving forward affects the x value
+    # But depending on the aim parameter, it can also affect the depth
+    class MoveForward
+      attr_reader :x, :depth
 
-    attr_writer :x, :depth
+      def initialize(units:, aim:)
+        @x = units.to_i
+        @depth = compute_depth_delta(units: units.to_i, aim: aim)
+      end
 
-    def parse(instructions)
-      direction, units = instructions.split(' ')
-      case direction
-      when 'forward'
-        self.x = units.to_i
-      when 'down'
-        self.depth = units.to_i
-      when 'up'
-        self.depth = -units.to_i
+      private
+
+      def compute_depth_delta(units:, aim:)
+        units * aim
       end
     end
   end
--- day_2_test.rb
+++ day_2_test.rb
@@ -3,6 +3,26 @@
 require 'minitest/autorun'
 require './day_2'
 
+class Day2MoveTest < MiniTest::Test
+  def test_move_forward_changes_x
+    move = Day2::Part1::Move.new('forward 5')
+    assert_equal 5, move.x
+    assert_equal 0, move.depth
+  end
+
+  def test_move_down_increments_depth
+    move = Day2::Part1::Move.new('down 5')
+    assert_equal 0, move.x
+    assert_equal 5, move.depth
+  end
+
+  def test_move_up_reduces_depth
+    move = Day2::Part1::Move.new('up 5')
+    assert_equal 0, move.x
+    assert_equal(-5, move.depth)
+  end
+end
+
 class Day2Test < MiniTest::Test
   DATA = <<~TEXT
     forward 5
@@ -13,30 +33,25 @@ class Day2Test < MiniTest::Test
     forward 2
   TEXT
 
-  def test_move_forward_changes_x
-    move = Day2::Move.new('forward 5')
-    assert_equal 5, move.x
-    assert_equal 0, move.depth
-  end
-
-  def test_move_down_increments_depth
-    move = Day2::Move.new('down 5')
-    assert_equal 0, move.x
-    assert_equal 5, move.depth
-  end
-
-  def test_move_up_reduces_depth
-    move = Day2::Move.new('up 5')
-    assert_equal 0, move.x
-    assert_equal(-5, move.depth)
+  def setup
+    @data = StringIO.new(DATA)
   end
 
   def test_returns_15_x_10_for_the_given_data
-    data = StringIO.new(DATA)
-    expected = { h: 15, depth: 10 }
-    assert_equal expected, Day2.new(data: data).compute_part_1!
+    expected = { x: 15, depth: 10 }
+    assert_equal expected, Day2.new(data: @data).part_1_position
   end
 
-  # def test_returns_five_for_the_given_data
-  # end
+  def test_returns_150_for_the_given_data
+    assert_equal 150, Day2.new(data: @data).compute_part_1!
+  end
+
+  def test_revised_version_returns_15_x_60_for_the_given_data
+    expected = { x: 15, depth: 60 }
+    assert_equal expected, Day2.new(data: @data).part_2_position
+  end
+
+  def test_revised_version_returns_900_for_the_given_data
+    assert_equal 900, Day2.new(data: @data).compute_part_2!
+  end
 end
