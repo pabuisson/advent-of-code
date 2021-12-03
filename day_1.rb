2e06e6a55b39551f4ade2a6e4b08f06137c74dd9
pabuisson <pierre-adrien.buisson@doctolib.com>
2021-12-03 19:40:00 +0100

    Day 1: part 2

--- day_1.rb
+++ day_1.rb
@@ -9,7 +9,7 @@ class Day1
     @data = data
   end
 
-  def compute!
+  def compute_part_1!
     compute_number_of_deeper_sonar_bips(data: data)
   end
 
@@ -23,6 +23,39 @@ class Day1
       end
 
     result[:times]
+  end
+
+  def deeper?(current:, previous:)
+    current > previous
+  end
+
+  def load_data
+    File.open('./day_1.txt').readlines.map(&:chomp).map(&:to_i)
+  end
+end
+
+class Day1Part2
+  attr_reader :data
+
+  def initialize(data: load_data)
+    @data = data
+  end
+
+  def compute!
+    compute_number_of_deeper_sonar_bips(data: data)
+  end
+
+  private
+
+  def compute_number_of_deeper_sonar_bips(data:)
+    acc = { last_sum: nil, times: 0 }
+    data.each_with_index do |_, index|
+      three_depths = data[index..index+2]
+      acc[:times] += 1 if acc[:last_sum] && deeper?(current: three_depths.sum, previous: acc[:last_sum])
+      acc[:last_sum] = three_depths.sum
+    end
+
+    acc[:times]
   end
 
   def deeper?(current:, previous:)
--- day_1_test.rb
+++ day_1_test.rb
@@ -4,9 +4,13 @@ require 'minitest/autorun'
 require './day_1'
 
 class Day1Test < MiniTest::Test
-  DATA = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+  DATA = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263].freeze
 
   def test_returns_seven_for_the_given_data
-    assert_equal 7, Day1.new(data: DATA).compute!
+    assert_equal 7, Day1.new(data: DATA).compute_part_1!
+  end
+
+  def test_returns_five_for_the_given_data
+    Day1Part2.new(data: DATA).compute!
   end
 end
