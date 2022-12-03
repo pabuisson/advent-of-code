516e6e7eb46462574c816ec63c9023d4f7632766
Pierre-Adrien <pabuisson@gmail.com>
2022-12-03 22:04:17 +0100

    chore: clearer test output

--- 2022/test/day_01_test.exs
+++ 2022/test/day_01_test.exs
@@ -2,7 +2,7 @@ defmodule Day01Test do
   use ExUnit.Case
 
   setup_all do
-    IO.puts("--- DAY 01 ----")
+    IO.puts("---- DAY 01 ----")
     {:ok, test_data} = File.read("data/day_01_part_01_test.txt")
     {:ok, real_data} = File.read("data/day_01_part_01.txt")
     {:ok, test_data: test_data, real_data: real_data}
@@ -11,13 +11,13 @@ defmodule Day01Test do
   describe "part 1" do
     test "test data", context do
       test_result = Day01.part_1(context[:test_data])
-      IO.puts("1/ test input: #{test_result}")
+      IO.puts("part 1 :: test : #{test_result}")
       assert test_result == 24_000
     end
 
     test "real data", context do
       real_result = Day01.part_1(context[:real_data])
-      IO.puts("1/ real input: #{real_result}")
+      IO.puts("part 1 :: real : #{real_result}")
       assert real_result
     end
   end
@@ -25,13 +25,13 @@ defmodule Day01Test do
   describe "part 2" do
     test "test data", context do
       test_result = Day01.part_2(context[:test_data])
-      IO.puts("2/ test input: #{test_result}")
+      IO.puts("part 2 :: test : #{test_result}")
       assert test_result == 45_000
     end
 
     test "real data", context do
       real_result = Day01.part_2(context[:real_data])
-      IO.puts("2/ real input: #{real_result}")
+      IO.puts("part 2 :: real : #{real_result}")
       assert real_result
     end
   end
--- 2022/test/day_02_test.exs
+++ 2022/test/day_02_test.exs
@@ -2,7 +2,7 @@ defmodule Day02Test do
   use ExUnit.Case
 
   setup_all do
-    IO.puts("--- DAY 02 ----")
+    IO.puts("---- DAY 02 ----")
     {:ok, test_data} = File.read("data/day_02_part_01_test.txt")
     {:ok, real_data} = File.read("data/day_02_part_01.txt")
     {:ok, test_data: test_data, real_data: real_data}
@@ -11,13 +11,13 @@ defmodule Day02Test do
   describe "part 1" do
     test "test data", context do
       test_result = Day02.part_1(context[:test_data])
-      IO.puts("1/ test input: #{test_result}")
+      IO.puts("part 1 :: test : #{test_result}")
       assert test_result == 15
     end
 
     test "real data", context do
       real_result = Day02.part_1(context[:real_data])
-      IO.puts("1/ real input: #{real_result}")
+      IO.puts("part 1 :: real : #{real_result}")
       assert real_result
     end
   end
@@ -25,13 +25,13 @@ defmodule Day02Test do
   describe "part 2" do
     test "test data", context do
       test_result = Day02.part_2(context[:test_data])
-      IO.puts("2/ test input: #{test_result}")
+      IO.puts("part 2 :: test : #{test_result}")
       assert test_result == 12
     end
 
     test "real data", context do
       real_result = Day02.part_2(context[:real_data])
-      IO.puts("2/ real input: #{real_result}")
+      IO.puts("part 2 :: real : #{real_result}")
       assert real_result
     end
   end
