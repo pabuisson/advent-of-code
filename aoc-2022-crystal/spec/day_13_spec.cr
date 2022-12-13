610471cc8327986f157009b24cd178cf797bc90f
Pierre-Adrien <pabuisson@gmail.com>
2022-12-13 18:38:59 +0100

    tests are green but actual data is not

--- aoc-2022-crystal/spec/day_13_spec.cr
+++ aoc-2022-crystal/spec/day_13_spec.cr
@@ -1,20 +1,20 @@
 require "./spec_helper"
 require "../src/day_13.cr"
 
-describe Aoc2022::Day13, focus: true do
+describe Aoc2022::Day13 do
   describe "part 1" do
-    # it "test data" do
-    #   test_data = File.read("./data/day_13_test.txt")
-    #   result = Aoc2022::Day13.part_1(input: test_data, log: false)
-    #   result.should eq 13
-    # end
+    it "test data" do
+      test_data = File.read("./data/day_13_test.txt")
+      result = Aoc2022::Day13.part_1(input: test_data, log: false)
+      result.should eq 13
+    end
 
-    # it "real data" do
-    #   real_data = File.read("./data/day_13.txt")
-    #   result = Aoc2022::Day13.part_1(input: real_data, log: false)
-    #   result.should be_truthy
-    #   result.should eq 182_293
-    # end
+    it "real data" do
+      real_data = File.read("./data/day_13.txt")
+      result = Aoc2022::Day13.part_1(input: real_data, log: false)
+      result.should be_truthy
+      # result.should eq 410
+    end
   end
 
   # describe "part 2" do
@@ -37,24 +37,78 @@ describe Aoc2022::Day13, focus: true do
       describe "flat array" do
         it "returns 1 if in right order" do
           result = Aoc2022::Day13::Pair.new("[1, 2, 3]", "[2, 3, 4]").right_order?
-          result.should eq 1
+          result.should be_true
         end
 
         it "returns 0 if not in right order" do
           result = Aoc2022::Day13::Pair.new("[1, 5, 3]", "[1, 3, 4]").right_order?
-          result.should eq 0
+          result.should be_false
         end
       end
 
       describe "one-level nested array" do
         it "returns 1 if sub-array in right order" do
           result = Aoc2022::Day13::Pair.new("[1, [2, 3]]", "[1, [3, 4]]").right_order?
-          result.should eq 1
+          result.should be_true
         end
 
         it "returns 0 if sub-array not in right order" do
-          result = Aoc2022::Day13::Pair.new("[1, [2, 3]]", "[1, [2, 4]]").right_order?
-          result.should eq 0
+          result = Aoc2022::Day13::Pair.new("[1, [2, 4]]", "[1, [2, 3]]").right_order?
+          result.should be_false
+        end
+      end
+
+      describe "multiple-level nested array" do
+        it "returns 1 if sub-array in right order" do
+          result = Aoc2022::Day13::Pair.new("[1, [2, [3]]]", "[1, [2, [4]]]").right_order?
+          result.should be_true
+        end
+
+        it "returns 0 if sub-array not in right order" do
+          result = Aoc2022::Day13::Pair.new("[1, [2, [4]]]", "[1, [2, [3]]]").right_order?
+          result.should be_false
+        end
+      end
+
+      describe "works for examples from the exercice" do
+        it "example 1" do
+          result = Aoc2022::Day13::Pair.new("[1, 1, 3, 1, 1]", "[1, 1, 5, 1, 1]").right_order?
+          result.should be_true
+        end
+
+        it "example 2" do
+          result = Aoc2022::Day13::Pair.new("[[1], [2, 3, 4]]", "[[1], 4]").right_order?
+          result.should be_true
+        end
+
+        it "example 3" do
+          result = Aoc2022::Day13::Pair.new("[9]", "[[8, 7, 6]]").right_order?
+          result.should be_false
+        end
+
+        it "example 4" do
+          result = Aoc2022::Day13::Pair.new("[[4, 4], 4, 4]", "[[4, 4], 4, 4, 4]").right_order?
+          result.should be_true
+        end
+
+        it "example 5" do
+          result = Aoc2022::Day13::Pair.new("[7, 7, 7, 7]", "[7, 7, 7]").right_order?
+          result.should be_false
+        end
+
+        it "example 6" do
+          result = Aoc2022::Day13::Pair.new("[]", "[3]").right_order?
+          result.should be_true
+        end
+
+        it "example 7" do
+          result = Aoc2022::Day13::Pair.new("[[[]]]", "[[]]").right_order?
+          result.should be_false
+        end
+
+        it "example 8" do
+          result = Aoc2022::Day13::Pair.new("[1,[2,[3,[4,[5,6,7]]]],8,9]", "[1,[2,[3,[4,[5,6,0]]]],8,9]").right_order?
+          result.should be_false
         end
       end
     end
--- aoc-2022-crystal/src/day_13.cr
+++ aoc-2022-crystal/src/day_13.cr
@@ -7,39 +7,52 @@ class Aoc2022::Day13
 
   def self.format(input)
     pairs = input.split("\n\n", remove_empty: true)
-    pairs.map do |p|
-      left, right = p.split("\n", remove_empty: true)
-      Pair.new(left_string: left, right_string: right)
-    end
+    r = pairs.map_with_index do |pair, index|
+      left, right = pair.split("\n", remove_empty: true)
+      if Pair.new(left_string: left, right_string: right).right_order?
+        index + 1
+      end
+    end.compact
+
+    p r
+
+    r.sum
   end
 
   struct Pair
     getter left, right
 
     def initialize(left_string, right_string)
-      @left = JSON.parse(left_string)
-      @right = JSON.parse(right_string)
+      @left = JSON.parse(left_string).as_a
+      @right = JSON.parse(right_string).as_a
     end
 
-    def right_order?(
-      left_json_array : JSON::Any = @left,
-      right_json_array : JSON::Any = @right
-    )
-      left_json_array.as_a.each_with_index do |l, i|
-        r = right_json_array.as_a[i]
+    def right_order?(left_json_array : Array(JSON::Any) = @left, right_json_array : Array(JSON::Any) = @right)
+      is_sorted = false
+
+      left_json_array.each_with_index do |l, i|
+        begin
+          r = right_json_array[i]
+        rescue
+          return is_sorted
+        end
 
         if l.as_i? && r.as_i?
-          return 0 if l.as_i > r.as_i
+          if l.as_i < r.as_i
+            is_sorted = true
+          elsif l.as_i > r.as_i
+            return false
+          end
         elsif l.as_i? && !r.as_i?
-          right_order?(left_json_array: JSON.parse("[#{l.as_i}]"), right_json_array: r.as_a)
+          return false unless right_order?(left_json_array: JSON.parse("[#{l.as_i}]").as_a, right_json_array: r.as_a)
         elsif !l.as_i? && r.as_i?
-          right_order?(left_json_array: l.as_a, right_json_array: JSON.parse("[#{r.as_i}]"))
+          return false unless right_order?(left_json_array: l.as_a, right_json_array: JSON.parse("[#{r.as_i}]").as_a)
         else
-          right_order?(left_json_array: l.as_a, right_json_array: r.as_a)
+          return false unless right_order?(left_json_array: l.as_a, right_json_array: r.as_a)
         end
       end
 
-      1
+      true
     end
   end
 end
