e403cc40828ac373935049ae3cd05f9aca4ba093
Pierre-Adrien Buisson <pabuisson@gmail.com>
2024-12-13 08:50:24 +0100

    2024: let's get started

---
+++ 2024/advent_day.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'debug'
+
+class AdventDay
+  def initialize(io: data_file)
+    @data = format_data(io: io)
+  end
+
+  private
+
+  attr_reader :data
+
+  def format_data(io:)
+    io.readlines.map(&:chomp).map do |chomped_line|
+      format_line(line: chomped_line)
+    end
+  end
+
+  def format_line(line:)
+    line
+  end
+
+  def data_file
+    File.open(data_filename)
+  rescue Errno::ENOENT => e
+    puts "Expected to find data file #{data_filename}, but it does not exist"
+  end
+
+  def data_filename
+    camel_case_full_class = self.class.name
+    camel_case_class = camel_case_full_class.split('::').last
+    snake_case_class = camel_case_class.gsub(/(.)([[:upper:]]|\d)/, '\1_\2').downcase
+    "data/#{snake_case_class}.txt"
+  end
+end
---
+++ 2024/advent_day_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require './test_helper'
+require './advent_day'
+
+class AdventDayTest < MiniTest::Test
+  class SomeTest1 < AdventDay
+  end
+
+  class SomeTest25 < AdventDay
+  end
+
+  def test_data_filename_is_infered_from_class_with_numbers
+    assert_equal 'data/some_test_1.txt', SomeTest1.new(io: StringIO.new('')).send(:data_filename)
+  end
+
+  def test_data_filename_is_infered_from_class_with_consecutive_numbers
+    assert_equal 'data/some_test_25.txt', SomeTest25.new(io: StringIO.new('')).send(:data_filename)
+  end
+end
