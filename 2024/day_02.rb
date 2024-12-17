0de12644d4b999f8fdd2e5de936c722a3a2d7b45
Pierre-Adrien Buisson <pabuisson@gmail.com>
2024-12-18 00:11:26 +0100

    2024 day 02: finally make it work 😰

--- 2024/day_02.rb
+++ 2024/day_02.rb
@@ -3,102 +3,47 @@
 require './advent_day'
 require 'debug'
 
+# Not sure why I found this one so, so difficult. It took me a long time to correctly understand the 2nd part,
+# and to finally reach a working solution. And I only managed to identify my algorithm problems by checking
+# the incorrect responses with a working solution, I'm not sure I would have found out without this 🤷
 class Day02 < AdventDay
-  def part_1!
-    @data.count { |report_levels| Report.new(report_levels).safe? }
-  end
-
-  def part_2!
-    @data.count { |report_levels| ReportWithDampener.new(report_levels).safe? }
-  end
+  def part_1! = @data.count { |levels| Report.new(levels).safe? }
+  def part_2! = @data.count { |levels| ReportWithDampener.new(levels).safe? }
 
   class Report
-    attr_reader :levels
-
     def initialize(levels_string)
       @levels = levels_string.split(' ').map(&:strip).map(&:to_i)
     end
 
-    def safe?
-      (increasing?(levels) || decreasing?(levels)) && in_range?(levels)
-    end
-
-    private
-
-    def increasing?(levels)
-      self.levels == levels.sort
-    end
-
-    def decreasing?(levels)
-      self.levels == levels.sort.reverse
-    end
-
-    def in_range?(levels)
-      levels.each_cons(2).all? { |a, b| (a - b).abs.between?(1, 3) }
-    end
+    def safe? = in_range?(@levels, (1..3)) || in_range?(@levels, (-3..-1))
+    def in_range?(levels, range) = levels.each_cons(2).all? { |a, b| range.include?(a - b) }
   end
 
   class ReportWithDampener
-    attr_reader :levels
-
     def initialize(levels_string)
       @levels = levels_string.split(' ').map(&:strip).map(&:to_i)
     end
 
-    def safe?
-      (increasing?(levels) && in_range?(levels, 1, 3)) || 
-       (decreasing?(levels) && in_range?(levels, -3, -1))
-    end
+    def safe? = in_range?(@levels, (1..3)) || in_range?(@levels, (-3..-1))
 
-    private 
-
-    def increasing?(levels)
-      exceptions_met = 0
-      levels.each_cons(2).each { |a, b| exceptions_met += 1 if a >= b }
-      (exceptions_met == 0 || exceptions_met == 1)
-    end
-
-    def decreasing?(levels)
-      exceptions_met = 0
-      levels.each_cons(2).each { |a, b| exceptions_met += 1 if a <= b }
-      (exceptions_met == 0 || exceptions_met == 1)
-    end
-
-    def in_range?(levels, min, max)
-      puts "--- #{levels} ----"
-      joker_used = false
+    def in_range?(levels, range, without_index: nil)
+      actual_levels = without_index.nil? ? levels : levels[...without_index] + levels[(without_index + 1)..]
 
       index = 1
-      while index < levels.size
-        base = levels[index - 1]
-        current = levels[index]
+      while index < actual_levels.size
+        previous = actual_levels[index - 1]
+        current = actual_levels[index]
 
-        if (current - base).between?(min, max)
-          puts "#{current} ok"
+        if range.include?(current - previous)
           index += 1
+        elsif without_index.nil?
+          # The dampening process
+          return in_range?(actual_levels, range, without_index: index) || in_range?(actual_levels, range, without_index: index - 1)
         else
-          puts "#{current} NOK (base)"
-
-          return false if joker_used
-          return true if index == levels.size - 1
-
-          next_value = levels[index + 1]
-          if (next_value - base).between?(min, max) || (next_value - current).between?(min, max)
-            puts "#{next_value} ok (joker used)"
-            joker_used = true
-            index = index + 2
-          else
-            puts "#{next_value} NOK (next)"
-            puts "--> NOPE"
-            return false
-          end
+          return false
         end
       end
 
-      # levels.each_cons(3).each do |a, b, c|
-      #   next if (a - b).between?(min, max) && (b - c).between?(min, max)
-      #   return false if !(a - c).between?(min, max)
-      # end
       true
     end
   end
--- 2024/day_02_test.rb
+++ 2024/day_02_test.rb
@@ -20,38 +20,76 @@ describe Day02 do
     assert_equal 2, Day02.new(io: @data).part_1!
   end
 
-  it 'returns 4 safe reports for part 2 with problem dampener' do
-    assert_equal 4, Day02.new(io: @data).part_2!
-  end
+  describe 'part 2' do
+    it 'returns 4 safe reports for part 2 with problem dampener' do
+      assert_equal 4, Day02.new(io: @data).part_2!
+    end
 
-  # Some specific cases
+    # Some specific cases
 
-  it 'return 0 for a report where more than two levels have same value' do
-    data = StringIO.new(<<~TEXT)
-    6 8 9 9 10 10
-    6 8 9 9 9 10
-    TEXT
+    it 'return 0 for a report where more than two levels have same value' do
+      data = StringIO.new("6 8 9 9 10 10")
+      assert_equal 0, Day02.new(io: data).part_2!
 
-    assert_equal 0, Day02.new(io: data).part_2!
-  end
+      data = StringIO.new("6 8 9 9 9 10")
+      assert_equal 0, Day02.new(io: data).part_2!
+    end
 
-  it 'return 1 for a report where last level is invalid' do
-    data = StringIO.new([6, 8, 9, 11, 10].join(' '))
-    assert_equal 1, Day02.new(io: data).part_2!
-  end
+    it 'return 1 for a report where last level is invalid' do
+      data = StringIO.new([6, 8, 9, 11, 10].join(' '))
+      assert_equal 1, Day02.new(io: data).part_2!
 
-  it 'return 0 for a report where last two levels are invalid' do
-    data = StringIO.new([6, 8, 1, 9, 11, 16].join(' '))
-    assert_equal 0, Day02.new(io: data).part_2!
-  end
+      data = StringIO.new([6, 8, 9, 11, 11].join(' '))
+      assert_equal 1, Day02.new(io: data).part_2!
 
-  it 'returns 1 for a report where first level is invalid' do
-    data = StringIO.new([3, 8, 9, 10, 11].join(' '))
-    assert_equal 1, Day02.new(io: data).part_2!
-  end
+      data = StringIO.new([6, 8, 9, 11, 18].join(' '))
+      assert_equal 1, Day02.new(io: data).part_2!
+    end
 
-  it 'return 0 for a report where two levels are invalid' do
-    data = StringIO.new([6, 8, 14, 10, 11, 17, 12, 13].join(' '))
-    assert_equal 0, Day02.new(io: data).part_2!
+    it 'return 1 for a report where 2nd-to-last level is invalid' do
+      data = StringIO.new([6, 8, 9, 11, 10, 12].join(' '))
+      assert_equal 1, Day02.new(io: data).part_2!
+
+      data = StringIO.new([6, 8, 9, 11, 11, 12].join(' '))
+      assert_equal 1, Day02.new(io: data).part_2!
+
+      data = StringIO.new([6, 8, 9, 11, 18, 12].join(' '))
+      assert_equal 1, Day02.new(io: data).part_2!
+    end
+
+    it 'return 0 for a report where last two levels are invalid' do
+      data = StringIO.new([5, 8, 11, 10, 9].join(' '))
+      assert_equal 0, Day02.new(io: data).part_2!
+
+      data = StringIO.new([5, 8, 11, 11, 11].join(' '))
+      assert_equal 0, Day02.new(io: data).part_2!
+
+      data = StringIO.new([5, 8, 11, 20, 30].join(' '))
+      assert_equal 0, Day02.new(io: data).part_2!
+    end
+
+    it 'returns 1 for a valid report, both increasing and decreasing' do
+      data = StringIO.new([5, 8, 11, 14, 16].join(' '))
+      assert_equal 1, Day02.new(io: data).part_2!
+
+      data = StringIO.new([12, 10, 9, 6, 4].join(' '))
+      assert_equal 1, Day02.new(io: data).part_2!
+    end
+
+    # NOTE: from my input, I was not identifying this one correctly as a success
+    describe 'missed cases from the inputs' do
+      it 'returns 1 for a report where one level is invalid' do
+        data = StringIO.new([18, 21, 23, 22, 23, 26, 28].join(' '))
+        assert_equal 1, Day02.new(io: data).part_2!
+      end
+
+      it 'returns 1 for a report where the first level only is invalid' do
+        data = StringIO.new([21, 24, 23, 21, 20, 19, 18].join(' '))
+        assert_equal 1, Day02.new(io: data).part_2!
+
+        data = StringIO.new([45, 52, 54, 56, 58, 60].join(' '))
+        assert_equal 1, Day02.new(io: data).part_2!
+      end
+    end
   end
 end
