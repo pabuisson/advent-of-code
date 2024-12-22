d2be9f0c17c8e48629d8f17753f365672e8b5426
Pierre-Adrien Buisson <pabuisson@gmail.com>
2024-12-22 19:22:37 +0100

    2024 day 12, part 2 : WIP

--- 2024/day_12.rb
+++ 2024/day_12.rb
@@ -5,7 +5,6 @@ require 'debug'
 require 'paint'
 
 class Day12 < AdventDay
-
   def part_1!
     regions = Map.new(@data).regions
 
@@ -22,9 +21,18 @@ class Day12 < AdventDay
 
   Position = Data.define(:line, :col) do
     def to_s = "(#{line},#{col})"
+    def top_left_corner = Position[line, col]
+    def top_right_corner = Position[line, col + 1]
+    def bottom_left_corner = Position[line + 1, col]
+    def bottom_right_corner = Position[line + 1, col + 1]
   end
 
   Plot = Data.define(:position, :letter)
+
+  Side = Data.define(:dir, :x, :y) do
+    def to_s = "#{symbol} from #{x} to #{y}"
+    def symbol = dir == :h ? '—' : '|'
+  end
 
   Region = Data.define(:positions, :letter) do
     def price = area * perimeter
@@ -37,6 +45,37 @@ class Day12 < AdventDay
           Position[p.line - 1, p.col],
           Position[p.line + 1, p.col]
         ].count { |p| !positions.include?(p) }
+      end
+    end
+
+    # FIXME: this is what I need for part 2.
+    #        my initial idea is to turn all positions into a segment, depending on the "free side" of the position,
+    #        if any.
+    #        then, 2nd step is to find the successive sides that have the same direction and a common beginning or end
+    #        BUT be careful of the cases where 2 squares "join" each other diagonally, like explained in today's description
+    #        or added in the tests
+    def sides
+      all_free_unit_sides =
+        positions
+          .uniq
+          .flat_map do |p|
+            left = Position[p.line, p.col - 1]
+            right = Position[p.line, p.col + 1]
+            top = Position[p.line - 1, p.col]
+            down = Position[p.line + 1, p.col]
+
+            free_sides = []
+            free_sides << Side[:v, p.top_left_corner, p.bottom_left_corner] if !positions.include?(left)
+            free_sides << Side[:v, p.top_right_corner, p.bottom_right_corner] if !positions.include?(right)
+            free_sides << Side[:h, p.top_left_corner, p.top_right_corner] if !positions.include?(top)
+            free_sides << Side[:h, p.bottom_left_corner, p.bottom_right_corner] if !positions.include?(down)
+
+            free_sides
+          end
+
+      processed_sides = []
+      # TODO: don't forget to "join" with the first side initial item?
+      while all_free_unit_sides.any?
       end
     end
   end
--- 2024/day_12_test.rb
+++ 2024/day_12_test.rb
@@ -27,6 +27,7 @@ describe Day12 do
   end
 
   describe 'part 1' do
+    focus
     it 'returns expected regions for small example' do
       Day12.new(io: @small).part_1! => {regions:, result:}
 
@@ -45,6 +46,7 @@ describe Day12 do
       assert_equal 140, result
     end
 
+    focus
     it 'returns expected regions for larger example' do
       Day12.new(io: @larger).part_1! => {regions:, result:}
 
@@ -78,5 +80,89 @@ describe Day12 do
   end
 
   describe 'part 2' do
+    focus
+    it 'returns the correct number of sides for small example' do
+      Day12.new(io: @small).part_2! => {regions:, result:}
+
+      {
+        "A" => 4,
+        "B" => 4,
+        "C" => 8,
+        "D" => 4,
+        "E" => 4
+      }.each do |letter, sides|
+        region = regions.find{ _1.letter == letter }
+        assert_equal sides, region.sides, "Expected sides for region #{letter} to be #{sides}, got #{region.sides}"
+      end
+
+      assert_equal 140, result
+    end
+
+    focus
+    it 'returns the correct number of sides for larger example' do
+      Day12.new(io: @larger).part_2! => {regions:, result:}
+
+      # Special case: 2 regions with the same letter
+      i_regions = regions.select { _1.letter == 'I' }.sort_by(&:area)
+      assert_equal [4, 16], i_regions.map(&:sides)
+
+      # Special case: 2 regions with the same letter
+      c_regions = regions.select { _1.letter == 'C' }.sort_by(&:area)
+      assert_equal [4, 22], c_regions.map(&:sides)
+
+      # Regular cases: regions with unique name
+      {
+        "R" => 10,
+        "F" => 12,
+        "V" => 10,
+        "J" => 12,
+        "E" => 8,
+        "M" => 6,
+        "S" => 6
+      }.each do |letter, sides|
+        region = regions.find{ _1.letter == letter }
+        assert_equal area, region.area, "Expected area for region #{letter} to be #{area}, got #{region.area}"
+        assert_equal sides, region.sides, "Expected sides for region #{letter} to be #{sides}, got #{region.sides}"
+      end
+
+      assert_equal 1_206, result
+    end
+
+    focus
+    it 'returns the correct result for a map with E shape' do
+      data = StringIO.new(<<~TEXT)
+      EEEEE
+      EXXXX
+      EEEEE
+      EXXXX
+      EEEEE
+      TEXT
+
+      Day12.new(io: data).part_2! => {regions:, result:}
+
+      region = regions.find { _1.letter == 'E' }
+      assert_equal 17, region.area
+      assert_equal 12, region.sides
+
+      assert_equal 236, result
+    end
+
+    # The B zones are adjacent diagonally
+    focus
+    it 'returns the correct result for a map with adjacent zones' do
+      data = StringIO.new(<<~TEXT)
+      AAAAAA
+      AAABBA
+      AAABBA
+      ABBAAA
+      ABBAAA
+      AAAAAA
+      TEXT
+
+      Day12.new(io: data).part_2! => {regions:, result:}
+
+      assert_equal 3, regions.count
+      assert_equal 368, result
+    end
   end
 end
