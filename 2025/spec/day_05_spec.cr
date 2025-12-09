1308aeb80062504fa536bbd96566bea671097f6b
Pierre-Adrien Buisson <pabuisson@gmail.com>
2025-12-09 13:41:36 +0100

    2025 day 05: part 2 🥵
    Trick was that one new fresh range may overlap / extend several existing
    ranges. So that's why all my initial solutions did not work, because of
    this case. The overlap cases were right, the merge was right, the size
    was right... only my input included such a case. That was super tricky
    to find out.

--- 2025/spec/day_05_spec.cr
+++ 2025/spec/day_05_spec.cr
@@ -17,14 +17,221 @@ describe Day05, tags: "day_05" do
   end
 
   describe "part 2" do
-    it "works with example data" do
+    describe Day05::FreshRange, tags: "support" do
+      describe "#overlaps_or_consecutive?" do
+        it "return false for distinct ranges" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(14, 18)
+
+          first.overlaps_or_consecutive?(second).should eq false
+          second.overlaps_or_consecutive?(first).should eq false
+        end
+
+        it "returns true for consecutive ranges" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(12, 18)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+
+          first = Day05::FreshRange.new(10, 18)
+          second = Day05::FreshRange.new(1, 10)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+        end
+
+        it "returns true for same range" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(1, 12)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+        end
+
+        it "returns true if one range is included in the other" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(5, 8)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+        end
+
+        it "returns true for regular overlaps" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(5, 14)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+
+          first = Day05::FreshRange.new(10, 18)
+          second = Day05::FreshRange.new(5, 14)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+        end
+
+        it "returns true for overlaps with same begin" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(1, 14)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(1, 20)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+        end
+
+        it "returns true for overlaps with same end" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(5, 12)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+
+          first = Day05::FreshRange.new(10, 18)
+          second = Day05::FreshRange.new(5, 18)
+
+          first.overlaps_or_consecutive?(second).should eq true
+          second.overlaps_or_consecutive?(first).should eq true
+        end
+      end
+
+      describe "#merge" do
+        it "raises for distinct ranges" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(14, 18)
+
+          expect_raises(Exception) do
+            first.merge(second)
+          end
+
+          expect_raises(Exception) do
+            second.merge(first)
+          end
+        end
+
+        it "merges two consecutive ranges" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(12, 18)
+
+          res = first.merge(second)
+          res.begin.should eq 1
+          res.end.should eq 18
+
+          res = second.merge(first)
+          res.begin.should eq 1
+          res.end.should eq 18
+        end
+
+        it "merges two same ranges into a single one" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(1, 12)
+
+          res = first.merge(second)
+          res.begin.should eq 1
+          res.end.should eq 12
+
+          res = second.merge(first)
+          res.begin.should eq 1
+          res.end.should eq 12
+        end
+
+        it "merges and extends the range if one range is included in the other" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(5, 8)
+
+          res = first.merge(second)
+          res.begin.should eq 1
+          res.end.should eq 12
+
+          res = second.merge(first)
+          res.begin.should eq 1
+          res.end.should eq 12
+        end
+
+        it "returns true for regular overlaps" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(5, 14)
+
+          res = first.merge(second)
+          res.begin.should eq 1
+          res.end.should eq 14
+
+          res = second.merge(first)
+          res.begin.should eq 1
+          res.end.should eq 14
+
+          first = Day05::FreshRange.new(10, 18)
+          second = Day05::FreshRange.new(5, 14)
+
+          res = first.merge(second)
+          res.begin.should eq 5
+          res.end.should eq 18
+
+          res = second.merge(first)
+          res.begin.should eq 5
+          res.end.should eq 18
+        end
+
+        it "returns true for overlaps with same begin" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(1, 14)
+
+          res = first.merge(second)
+          res.begin.should eq 1
+          res.end.should eq 14
+
+          res = second.merge(first)
+          res.begin.should eq 1
+          res.end.should eq 14
+        end
+
+        it "returns true for overlaps with same end" do
+          first = Day05::FreshRange.new(1, 12)
+          second = Day05::FreshRange.new(5, 12)
+
+          res = first.merge(second)
+          res.begin.should eq 1
+          res.end.should eq 12
+
+          res = second.merge(first)
+          res.begin.should eq 1
+          res.end.should eq 12
+        end
+      end
+
+      describe "#size" do
+        it "return 1 if begin and end are the same" do
+          Day05::FreshRange.new(10, 10).size.should eq 1
+        end
+
+        it "return the number of items for regular ranges" do
+          # 1, 2, 3, 4
+          Day05::FreshRange.new(1, 4).size.should eq 4
+        end
+      end
+    end
+
+    it "works with example data", tags: "focus" do
       test_data = File.read_lines("./data/day_05_test.txt")
       Day05.part_2(test_data, must_log: false).should eq 14
     end
 
+    it "works with custom data" do
+      data = ["1-2", "2-2", "2-4", "10-12", "9-13"]
+      Day05.part_2(data, must_log: false).should eq 4 + 5
+
+      data = ["2-3", "5-6", "1-8", "5-10", "6-12"]
+      Day05.part_2(data, must_log: false).should eq 12
+    end
+
     it "works with normal data" do
       test_data = File.read_lines("./data/day_05.txt")
-      Day05.part_2(test_data).should eq nil
+      Day05.part_2(test_data, must_log: false).should eq 352_681_648_086_146
     end
   end
 end
--- 2025/src/day_05.cr
+++ 2025/src/day_05.cr
@@ -2,46 +2,91 @@ require "./base"
 
 class Day05 < Base
   struct FreshRange
-    property range : Range(Int64, Int64)
+    property range : Range(Int128, Int128)
 
     def initialize(range : String)
-      from, to = range.split("-").map(&.to_i64)
+      from, to = range.split("-").map(&.to_i128)
       @range = Range.new(from, to)
     end
 
-    delegate includes?, to: @range
-    delegate to_a, to: @range
+    def initialize(from : Int128, to : Int128)
+      @range = Range.new(from, to)
+    end
+
+    delegate :includes?, to: @range
+    delegate :begin, to: @range
+    delegate :end, to: @range
+    delegate :size, to: @range
+
+    def overlaps_or_consecutive?(other : FreshRange)
+      consecutive?(other) || overlaps?(other)
+    end
+
+    private def consecutive?(other : FreshRange)
+      (other.begin == self.end && other.end >= self.end) ||
+        (other.end == self.begin && other.begin <= self.begin)
+    end
+
+    private def overlaps?(other : FreshRange)
+      (other.begin < self.begin && other.end > self.begin) ||
+        (other.begin < self.end && other.end > self.end) ||
+        (other.begin >= self.begin && other.end <= self.end)
+    end
+
+    # Extends self with the new range.
+    # self and other must overlap or be immediately consecutive, otherwise will raise an exception
+    def merge(other : FreshRange) : FreshRange
+      raise "#{self} and #{other} are disjoint, can't merge them" unless overlaps_or_consecutive?(other)
+      FreshRange.new([self.begin, other.begin].min, [self.end, other.end].max)
+    end
+
+    # Need to override this: Range.size raises an OverflowError
+    # +1 because it must count the number of items with both ends included
+    def size
+      (@range.end - @range.begin) + 1
+    end
+
+    def to_s(io : IO)
+      io << "FreshRange(#{@range.begin.format} → #{@range.end.format})"
+    end
   end
 
-  def self.parse_input(input : Array(String)) : {ranges: Array(FreshRange), ingredients: Array(Int64)}
-    input.reduce({ranges: [] of FreshRange, ingredients: [] of Int64}) do |acc, line|
+  def self.parse(input : Array(String)) : {ranges: Array(FreshRange), ingredients: Array(Int128)}
+    input.reduce({ranges: [] of FreshRange, ingredients: [] of Int128}) do |acc, line|
       next acc if line.empty?
 
       if line.includes?("-")
         {ranges: acc[:ranges] << FreshRange.new(line), ingredients: acc[:ingredients]}
       else
-        {ranges: acc[:ranges], ingredients: acc[:ingredients] << line.to_i64}
+        {ranges: acc[:ranges], ingredients: acc[:ingredients] << line.to_i128}
       end
     end
   end
 
   def self.part_1(input, must_log = false)
-    data = parse_input(input)
+    data = parse(input)
     data[:ingredients].count do |ingredient|
       data[:ranges].any? { |range| range.includes?(ingredient) }
     end
   end
 
-  # FIXME: does not work for real data. Stuck on the very first range.
-  #        I must turn the collection of ranges in a collection of "disjoint" ranges.
-  #        Then, I'll just have to sum the ranges size.
-  # TODO: process the ranges so that they're all exclusive and I can just add their size
   def self.part_2(input, must_log = false)
-    data = parse_input(input)
-    puts "Number of FreshRanges: #{data[:ranges].size}"
-    data[:ranges].flat_map { |range|
-      puts "Range #{range}..."
-      range.to_a
-    }.uniq.size
+    data = parse(input)
+    puts "Number of FreshRanges: #{data[:ranges].size}" if must_log
+
+    data[:ranges]
+      .reduce([] of FreshRange) do |acc, fr|
+        if must_log
+          puts "⚙️ Processing range #{fr}"
+          puts "   Current ranges: #{acc.map(&.to_s)}"
+        end
+
+        to_process, to_keep_untouched = acc.partition { |range| range.overlaps_or_consecutive?(fr) }
+
+        puts "    #{to_process.size} ranges to merge, #{to_keep_untouched.size} ranges to leave alone" if must_log
+
+        to_keep_untouched << to_process.reduce(fr) { |acc, range_to_process| acc.merge(range_to_process) }
+      end
+      .sum { |fr| fr.size }
   end
 end
